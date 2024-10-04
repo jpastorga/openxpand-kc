a65d61124c18347bdadfc6297a87abb575649d05
Juan Pablo Astorga <juan.astorga@intraway.com>
2024-10-04 12:15:25 -0300

    changed: rename file

--- pages/api/echo.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-    if (req.method === 'GET') {
-      res.status(200).json({ message: req.query?.code });
-    } else {
-      res.status(405).json({ message: 'Not Allowed' });
-    }
-}
