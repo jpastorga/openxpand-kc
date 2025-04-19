5a85670d17e44bb69c79d85ce0f370adf0d7f326
Juan Pablo Astorga <juan.astorga@intraway.com>
2025-04-19 13:18:05 -0300

    added: security policy

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,16 @@
 import type { NextConfig } from "next";
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval';
+  style-src 'self' 'unsafe-inline';
+  img-src * data: blob:;
+  font-src 'self';
+  connect-src 'self';
+  object-src 'none';
+  frame-ancestors 'none';
+`;
+
 const nextConfig: NextConfig = {
   async headers() {
     return [
@@ -9,6 +20,22 @@ const nextConfig: NextConfig = {
           {
             key: "Strict-Transport-Security",
             value: "max-age=31536000; includeSubDomains; preload"
+          },
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\n/g, "").trim()
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff"
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY"
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin"
           }
         ]
       }
